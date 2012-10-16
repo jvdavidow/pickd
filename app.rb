0b8d0ddba5aed3d26b909ea04ed22de7a83b2134
jon davidow <jvdavidow@gmail.com>
2012-10-16 01:10:59 -0400

    intro to sinatra

--- app.rb
+++ app.rb
@@ -1,5 +1,22 @@
 require 'sinatra'
 
 get '/' do
+  @name = "George"
   erb :index
+end
+
+get '/login' do
+  erb :login
+end
+
+post '/login/:name' do
+  @name={:name}
+  "Welcome back, #{params[:name]}"
+
+get '/connect' do
+  erb :connect
+end
+
+get '/calc' do
+  erb :calc
 end
